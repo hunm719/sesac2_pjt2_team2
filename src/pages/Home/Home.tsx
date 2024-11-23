0306068d4a0c3b54b05dc26eb193da22d403c996
Suin Kang <flqpddl2@gmail.com>
2024-11-23 16:15:39 +0900

    Initial commit with React app in frontend folder

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Home() {
+  return (
+    <>
+      {/* 컴포넌트 화 시킬 수 있으면 시키기 - 재사용성 */}
+      <div>내 강아지 자랑</div>
+
+      <div>게시판</div>
+    </>
+  )
+}
+export default Home
