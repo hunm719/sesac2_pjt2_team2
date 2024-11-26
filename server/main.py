389873dbc72e6efb1dbf133f2a26bc5f91c52940
hunm719 <hunm719@gmail.com>
2024-11-26 12:16:50 +0900

    main.py 수정

--- server/main.py
+++ server/main.py
@@ -10,3 +10,11 @@ app = FastAPI()
 
 # 게시글 관련 라우터 추가
 app.include_router(post_routes.router, prefix="/posts", tags=["posts"])
+
+# User 관련 라우터 추가
+app.include_router(user.router, prefix="/api/users", tags=["Users"])
+
+# 응답 예시
+@app.get("/")
+def read_root():
+    return {"message": "fastapi가 잘 작동됩니다"}
