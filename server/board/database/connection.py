9205e65ffa725ebe2a1c2cc327432c5f216c31b3
hunm719 <hunm719@gmail.com>
2024-11-27 10:59:31 +0900

    유저 기능 복구

--- server/board/database/connection.py
+++ server/board/database/connection.py
@@ -1,19 +1,19 @@
 from sqlmodel import create_engine, SQLModel, Session
 
-database_file = "database.db"
-database_connection_string = f"sqlite:///{database_file}"
-connect_args = {"check_same_thread": False}
-engine_url = create_engine(
-    database_connection_string, echo=True, connect_args=connect_args
-)
-
+class Settings:
+    # 데이터베이스 파일과 연결 문자열을 포함한 설정
+    database_file = "database.db"
+    database_connection_string = f"sqlite:///{database_file}"
+    connect_args = {"check_same_thread": False}
+    engine_url = create_engine(
+        database_connection_string, echo=True, connect_args=connect_args
+    )
 
 def conn():
     # SQLModel을 상속받은 모든 클래스를 기반으로 데이터베이스에 테이블을 생성
-    SQLModel.metadata.create_all(engine_url)
-
+    SQLModel.metadata.create_all(Settings.engine_url)
 
 def get_session():
     # Session => 데이터베이스와 상호작용(CRUD)을 관리하는 객체
-    with Session(engine_url) as session:
+    with Session(Settings.engine_url) as session:
         yield session
